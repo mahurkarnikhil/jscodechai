658ad4008c401ef9d5888b5fa5c3be6a35b136da
Nikhil Mahurkar <114988144+mahurkarnikhil@users.noreply.github.com>
2025-01-04 17:29:35 +0000

    datatypes and variables

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,16 @@
+// use strict == treat all js code as newer version
+// alert (3+3) // we arte using node.js , not browser 
+
+let name = 'nkhil'
+
+console.log(typeof nikhil);
+
+
+let name1 = "bipin"
+console.log(typeof bipin);
+
+console.log(typeof name);
+
+
+
+
---
+++ 01_basic/variables.js
@@ -0,0 +1,8 @@
+const nikhil = "25366";
+let mahurkar = "89";
+var surname = "dodo";
+
+console.log(nikhil);
+console.log(mahurkar);
+console.log(surname);
+
